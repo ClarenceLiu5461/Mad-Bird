e50cb43583159e28ea0c654dc28c38169696e3e1
ClarenceLiu5461 <frank1213450@gmail.com>
2023-07-02 23:21:36 +0800

    Clamping position with Vector2  distance

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,21 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] float launchForce = 500;
+    [SerializeField] float maxDragDistance = 1;
     Vector2 startPostion;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigid2D;
     SpriteRenderer spriteRenderer;
 
     void Awake()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigid2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
-        startPostion = rigidbody2D.position;
-        rigidbody2D.isKinematic = true;
+        startPostion = GetComponent<Rigidbody2D>().position;
+        GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
     void OnMouseDown()
@@ -25,22 +28,47 @@ public class Bird : MonoBehaviour
 
     void OnMouseUp()
     {
-        Vector2 currentPostion = rigidbody2D.position;
+        Vector2 currentPostion = rigid2D.position;
         Vector2 direction = startPostion - currentPostion;
         direction.Normalize();
-        rigidbody2D.isKinematic = false;
-        rigidbody2D.AddForce(direction * 500);
+        rigid2D.isKinematic = false;
+        rigid2D.AddForce(direction * launchForce);
         spriteRenderer.color = Color.white;
     }
 
     void OnMouseDrag()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
+        Vector2 desiredPosition = mousePosition;
+        float distance = Vector2.Distance(desiredPosition, startPostion);
+        if (distance > maxDragDistance)
+        {
+            Vector2 direction = desiredPosition - startPostion;
+            direction.Normalize();
+            desiredPosition = startPostion + (direction * maxDragDistance);
+        }
+        if (desiredPosition.x > startPostion.x)
+        {
+            desiredPosition.x = startPostion.x;
+        }
+        rigid2D.position = desiredPosition;
     }
 
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        rigid2D.position = startPostion;
+        rigid2D.isKinematic = true;
+        rigid2D.velocity = Vector2.zero;
     }
 }
